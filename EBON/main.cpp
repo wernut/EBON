407a0692aaeccb1bd3b73a5665e303a8321e8875
Wernut <wernert97@live.com>
2020-02-03 16:52:46 +1100

    hacky pvm added, getting removed tomorrow

--- EBON/main.cpp
+++ EBON/main.cpp
@@ -1,4 +1,5 @@
 #include "glm.hpp"
+#include "ext.hpp"
 #include "..\glcore\gl_core_4_5.h"
 #include "glfw3.h"
 
@@ -47,19 +48,33 @@ int main()
 		glm::vec3( 0.5f, -0.5f, 0.0f),
 	};
 
-	// Making our buffers:
-	uint VAO;
-	uint VBO;
-	//uint IBO;
+	// hacky projection and view matricies
+	glm::mat4 projection = glm::perspective(90.0f, 16 / 9.0f, 0.1f, 5.0f);
+	glm::mat4 view = glm::lookAt(glm::vec3(0, 0, 1), glm::vec3(0), glm::vec3(0, 1, 0));
+	glm::mat4 model = glm::mat4(1);
 
+	glm::mat4 pvm = projection * view * model;
+
+	for (int i = 0; i < 6; ++i)
+	{
+		verticies[i] = glm::vec3(pvm * glm::vec4(verticies[i], 1));
+	}
+
+	// declaring the buffers:
+	uint VAO;	// vertex array object
+	uint VBO;	// vertex buffer object
+	//uint IBO; // index buffer object
+
+	// generating vao & vbo
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
+	// binding vao and vbo
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(glm::vec3), &verticies[0], GL_STATIC_DRAW);
 
-	// creating vertix attrib pointer
+	// creating vertix attribute pointer
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
 
@@ -67,11 +82,10 @@ int main()
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-
 	// Entering a while loop until an exit event has been registered:
 	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
-		// Render code and game logic here!
+		// Drawing test triangle:
 		glClearColor(0.25f, 0.25f, 0.25f, 1);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
