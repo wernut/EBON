35a35c7cea17573a3998c36c8e564db646ff6320
Wernut <wernert97@live.com>
2020-02-03 15:48:47 +1100

    Added: window, loop & testing of opengl functions.

--- EBON/main.cpp
+++ EBON/main.cpp
@@ -2,6 +2,8 @@
 #include "..\glcore\gl_core_4_5.h"
 #include "glfw3.h"
 
+using uint = unsigned int;
+
 int main()
 {
 	// Initalise GLFW:
@@ -34,10 +36,47 @@ int main()
 	auto minor = ogl_GetMinorVersion();
 	printf("GL: %i.%i\n", major, minor);
 
+	// Quad vertices:
+	glm::vec3 verticies[]
+	{
+		glm::vec3(-0.5f,  0.5f, 0.0f),
+		glm::vec3( 0.5f,  0.5f, 0.0f),
+		glm::vec3(-0.5f, -0.5f, 0.0f),
+		glm::vec3( 0.5f,  0.5f, 0.0f),
+		glm::vec3(-0.5f, -0.5f, 0.0f),
+		glm::vec3( 0.5f, -0.5f, 0.0f),
+	};
+
+	// Making our buffers:
+	uint VAO;
+	uint VBO;
+	//uint IBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(glm::vec3), &verticies[0], GL_STATIC_DRAW);
+
+	// creating vertix attrib pointer
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+
+	// unbinding
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
 	// Entering a while loop until an exit event has been registered:
 	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
 		// Render code and game logic here!
+		glClearColor(0.25f, 0.25f, 0.25f, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		// Updating the monitors display by swapping the back buffer:
 		glfwSwapBuffers(window);
